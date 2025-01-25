9303f994bb3da44ff575414710bcaf000b1597c6
kawen25 <146899091+kawen25@users.noreply.github.com>
2025-01-24 22:35:19 -0800

    dark mode implementation

--- global.js
+++ global.js
@@ -51,3 +51,32 @@ for (let p of pages) {
   // Append the link to the nav
   nav.append(a);
 }
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
+);
+
+const select = document.querySelector('.color-scheme select');
+
+select.addEventListener('input', function (event) {
+  const colorScheme = event.target.value;
+  document.documentElement.style.setProperty('color-scheme', colorScheme);
+  localStorage.colorScheme = colorScheme; // Save preference
+});
+
+// Load saved preference on page load
+const savedColorScheme = localStorage.colorScheme;
+if (savedColorScheme) {
+  document.documentElement.style.setProperty('color-scheme', savedColorScheme);
+  select.value = savedColorScheme; // Update dropdown to match
+}
