5c65b18204cd802bcf6d641258e13a2522cf7a90
kawen25 <146899091+kawen25@users.noreply.github.com>
2025-01-31 23:27:16 -0800

    fixing home again

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const githubData = await fetchGitHubData('kawen25');
 const profileStats = document.querySelector('#profile-stats');
 
 
-if (profileStats) {
-    profileStats.innerHTML = `
-          <dl>
-            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-            <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-            <dt>Followers:</dt><dd>${githubData.followers}</dd>
-            <dt>Following:</dt><dd>${githubData.following}</dd>
-          </dl>
-      `;
-  }
+const githubStatsContent = document.querySelector('#github-stats-content');
+if (githubStatsContent) {
+  githubStatsContent.innerHTML = `
+    <dl>
+      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+      <dt>Followers:</dt><dd>${githubData.followers}</dd>
+      <dt>Following:</dt><dd>${githubData.following}</dd>
+    </dl>
+  `;
+}
--- projects/projects.js
+++ projects/projects.js
@@ -6,12 +6,9 @@ import { fetchJSON, renderProjects } from '../global.js';
   const projectsTitle = document.querySelector('.projects-title');
 
   // Update the title with the number of projects
-  projectsTitle.textContent += ` (${projects.length} projects)`;
-
   if (projectsTitle) {
-    projectsTitle.textContent += ` (${projects.length} projects)`;
-  } else {
-    console.error('Projects title element not found!');
-  }
+    projectsTitle.innerHTML = `${projects.length} Projects`;
+}
+  renderProjects(projects, projectsContainer, 'h2');
 
 })();
