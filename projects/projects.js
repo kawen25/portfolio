f3f10defd646df13eca0e26d4cc6a0553f53fcb8
kawen25 <146899091+kawen25@users.noreply.github.com>
2025-01-31 23:32:25 -0800

    still fixing

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
-(async function () {
   const projects = await fetchJSON('../lib/projects.json');
   const projectsContainer = document.querySelector('.projects');
   const projectsTitle = document.querySelector('.projects-title');
@@ -10,5 +9,3 @@ import { fetchJSON, renderProjects } from '../global.js';
     projectsTitle.innerHTML = `${projects.length} Projects`;
 }
   renderProjects(projects, projectsContainer, 'h2');
-
-})();
