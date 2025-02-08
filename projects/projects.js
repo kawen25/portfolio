cc0795ab3e290edbd8a09219ab6ef781db7ee95d
kawen25 <146899091+kawen25@users.noreply.github.com>
2025-02-07 19:50:33 -0800

    adding legend for pie chart

--- projects/projects.js
+++ projects/projects.js
@@ -23,10 +23,17 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
   });
   
 
-let data = [1, 2, 3, 4, 5, 5];
-let colors = ["gold", "purple"];
+  let data = [
+    { value: 1, label: 'apples' },
+    { value: 2, label: 'oranges' },
+    { value: 3, label: 'mangos' },
+    { value: 4, label: 'pears' },
+    { value: 5, label: 'limes' },
+    { value: 5, label: 'cherries' },
+  ];
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map(d => arcGenerator(d));
 
@@ -34,5 +41,12 @@ arcs.forEach((arc, idx) => {
   d3.select("#projects-pie-plot")
     .append("path")
     .attr("d", arc)
-    .attr("fill", colors[idx]);
+    .attr("fill", colors(idx));
 });
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+    legend.append('li')
+          .attr('style', `--color:${colors(idx)}`) 
+          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); 
+})
