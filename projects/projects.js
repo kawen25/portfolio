42d941055298e2ad7f9320e0f87f2f9a247010c2
kawen25 <146899091+kawen25@users.noreply.github.com>
2025-02-07 18:42:34 -0800

    making pie

--- projects/projects.js
+++ projects/projects.js
@@ -9,3 +9,30 @@ import { fetchJSON, renderProjects } from '../global.js';
     projectsTitle.innerHTML = `${projects.length} Projects`;
 }
   renderProjects(projects, projectsContainer, 'h2');
+
+
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+
+  const arcGenerator = d3.arc()
+    .innerRadius(0) 
+    .outerRadius(50); 
+  
+  const arc = arcGenerator({
+    startAngle: 0, 
+    endAngle: 2 * Math.PI, 
+  });
+  
+
+let data = [1, 2];
+let colors = ["gold", "purple"];
+
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map(d => arcGenerator(d));
+
+arcs.forEach((arc, idx) => {
+  d3.select("#projects-pie-plot")
+    .append("path")
+    .attr("d", arc)
+    .attr("fill", colors[idx]);
+});
